b3af2a75052c912a55351de6837dac865dc729a3
SiGuyHere <siiguyhere@gmail.com>
2026-09-17 11:15:14 -0600

    pondering

--- ponder/js-variables.js
+++ ponder/js-variables.js
@@ -8,3 +8,15 @@ age = 34;
 console.log(age);
 
 const eyeColor = "blue";
+const policyDisclaimers = "Exclusions apply to vehicles registered in Michigan";
+
+if(age == 34) {
+
+    console.log(name);
+
+    favoriteColor = "blue";
+}
+
+console.log(favoriteColor);
+
+document.querySelector("h1").style.color = favoriteColor;
